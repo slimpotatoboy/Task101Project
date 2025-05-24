6ed58ffd4b472508a93457d4f1666731ff488287
slimpotatoboy <edwinmhzn@gmail.com>
2025-05-25 00:46:38 +1000

    test failed updated

--- src/main/java/sit707/TaskService.java
+++ src/main/java/sit707/TaskService.java
@@ -6,7 +6,7 @@ public class TaskService {
     public static List<Task> getSubmittedTasks(String studentId) {
         // Mocked task list based on studentId
         List<Task> tasks = new ArrayList<>();
-        if ("s123".equals(studentId)) {
+        if ("SIT12".equals(studentId)) {
             tasks.add(new Task("7P", "Task 1", "Marked"));
             tasks.add(new Task("8P", "Task 2", "Pending Feedback"));
         }
